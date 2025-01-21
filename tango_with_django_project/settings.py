7461bf2295ec704914a576eb1a7c5509e2f01585
Lewis Ng <2884302n@student.gla.ac.uk>
2025-01-21 15:54:39 +0000

    views and url mapping

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xp91dj^%e04$c@rmq*$+w=&ftge^nm36(xhj2d9os1rg-&z=r@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000']
+ALLOWED_HOSTS = []
 
 
 # Application definition
